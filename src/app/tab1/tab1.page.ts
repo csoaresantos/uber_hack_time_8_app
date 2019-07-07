6bb8479addbe432b34f33964a49da1fc8b0de3a8
Italo Lelis <italohdc@gmail.com>
2019-07-07 13:35:19 -0300

    Update avatar

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { AcompanhanteService, Acompanhante } from 'src/app/services/acompanhante.service';
-=======
-import { IdosoService, Idoso } from 'src/app/services/idoso.service';
->>>>>>> service-idoso
 import { Observable } from 'rxjs';
 
 @Component({
@@ -13,11 +8,8 @@ import { Observable } from 'rxjs';
 })
 export class Tab1Page implements OnInit {
 
-  private idosos: Observable<Acompanhante[]>;
-
-  constructor(private ideaService: AcompanhanteService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.idosos = this.ideaService.getAcompanhantes();
   }
 }
