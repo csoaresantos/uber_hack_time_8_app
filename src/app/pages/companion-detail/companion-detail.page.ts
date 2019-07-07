c0c6abcde65d90debc50082e7669ad63704c1bd5
Italo Lelis <italohdc@gmail.com>
2019-07-07 05:16:22 -0300

    Create page companion-detail

---
+++ src/app/pages/companion-detail/companion-detail.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-companion-detail',
+  templateUrl: './companion-detail.page.html',
+  styleUrls: ['./companion-detail.page.scss'],
+})
+export class CompanionDetailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
