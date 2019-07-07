f1793b3752aee8111881896b66db47884e7daf0f
Italo Lelis <italohdc@gmail.com>
2019-07-07 07:04:48 -0300

    Create page my-events

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'companion-detail', loadChildren: './pages/companion-detail/companion-detail.module#CompanionDetailPageModule' },
   { path: 'relative-detail', loadChildren: './pages/relative-detail/relative-detail.module#RelativeDetailPageModule' },
-  { path: 'elderly-detail', loadChildren: './pages/elderly-detail/elderly-detail.module#ElderlyDetailPageModule' }
+  { path: 'elderly-detail', loadChildren: './pages/elderly-detail/elderly-detail.module#ElderlyDetailPageModule' },
+  { path: 'my-events', loadChildren: './pages/my-events/my-events.module#MyEventsPageModule' }
 ];
 @NgModule({
   imports: [
---
+++ src/app/pages/my-events/my-events.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-events',
+  templateUrl: './my-events.page.html',
+  styleUrls: ['./my-events.page.scss'],
+})
+export class MyEventsPage implements OnInit {
+
+  events = [
+    {
+      name: 'Consulta Médica',
+      _elderly: {},
+      date: new Date('2019-07-10T09:30:00.000Z'),
+      details: 'Mandar foto do laudo médico',
+      address: 'Hospital Mater Dei',
+      _companion: {},
+      status: 'on',
+    },
+    {
+      name: 'Consulta Médica',
+      _elderly: {},
+      date: new Date('2019-07-11T09:30:00.000Z'),
+      details: 'Mandar foto do laudo médico',
+      address: 'Hospital Aliança',
+      _companion: {},
+      status: 'on',
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
